52df495d1d79429a85060379860cda941dd21770
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-19 14:04:33 +0100

    First DB Connection with PDO

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+
+
+// Connect to MySQL database.
+
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}
