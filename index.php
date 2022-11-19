23178cb5d6f0d2705c449ae4167f35efa9581f31
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-19 14:45:13 +0100

    Started refactoring DB connection

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <?php
 require 'functions.php';
 // require 'router.php';
+require 'Database.php';
 
 
+// Create an instance of a new DB class.
+
+$db = new Database();
+
+$posts = $db->query("select * from posts")->fetch(PDO::FETCH_ASSOC);
 
 // Connect to MySQL database.
-
-$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 
 foreach ($posts as $post) {
   echo "<li>" . $post['title'] . "</li>";
