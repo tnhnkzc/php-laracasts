69565778b450991ca06c4112b21efbd6da782a1b
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-19 15:30:25 +0100

    Continue refactoring DB connection

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ require 'functions.php';
 // require 'router.php';
 require 'Database.php';
 
+$config = require('config.php');
 
 // Create an instance of a new DB class.
 
-$db = new Database();
+$db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetch(PDO::FETCH_ASSOC);
+$posts = $db->query("select * from posts")->fetchAll();
 
 // Connect to MySQL database.
 
