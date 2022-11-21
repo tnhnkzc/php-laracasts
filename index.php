9ada463015eabb923067d4eb965e479f941382b1
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-21 21:49:34 +0100

    SQL injection vulnerabilites

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+$query = "select * from posts where id=:id";
+
+
+$posts = $db->query($query, ['id' => $id])->fetchAll();
 
 // Connect to MySQL database.
 
